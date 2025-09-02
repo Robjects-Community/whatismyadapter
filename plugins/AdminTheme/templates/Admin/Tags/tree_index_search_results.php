42b6e301b4005e1101fa835d7c96d37a7450145b
Mike <garzarobm@gmail.com>
2025-09-02 02:55:09 -0500

    Remove favicon and color mode toggler JavaScript from DefaultTheme plugin

--- plugins/AdminTheme/templates/Admin/Tags/tree_index_search_results.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if (!empty($tags)) {
-        echo $this->element('tree/tag_tree', ['tags' => $tags, 'level' => 0]);
-    } else {
-        echo $this->Html->tag('p', __('No tags found.'));
-    }
-?>
